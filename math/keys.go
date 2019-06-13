c0af43b07bff2c29f1811ac4956635b91a8d30fc
leonardo <leo.braga95@gmail.com>
2019-06-13 15:18:19 +0200

    keys development done. need to be HAARD TESTED

--- math/keys.go
+++ math/keys.go
@@ -1,21 +1,42 @@
 package math
 
 import (
+	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
 	"goUmbral/openssl"
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/hkdf"
 	"golang.org/x/crypto/nacl/secretbox"
 	"golang.org/x/crypto/scrypt"
+	"hash"
 	"math"
 	"strconv"
 )
 
-const SALT_SIZE = 32
-const DEFAULT_SCRYPT_COST = 20
-const SYMMETRIC_KEY_SIZE = 128
-const NONCE_SIZE = 64
+const (
+	SALT_SIZE           = 32
+	DEFAULT_SCRYPT_COST = 20
+	SYMMETRIC_KEY_SIZE  = 128
+	NONCE_SIZE          = 64
+)
+
+func generateRandNonce() [24]byte {
+
+	nonce := make([]byte, NONCE_SIZE)
+	_, err := rand.Read(nonce)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var hNonce [24]byte
+	copy(hNonce[:], nonce[:])
+
+	return hNonce
+}
 
 /**
 Derives a symmetric encryption key from a pair of password and salt.
@@ -61,7 +82,7 @@ Alternatively, it can derive the wrapping key from a password.
 */
 //TODO look at pyUmbral keys function and nacl.secretbox python implementation,
 // can nonce be null? if so, delete that from params
-func WrapKey(keyToWrap []byte, wrappingKey []byte, password []byte, nonce [24]byte) []byte {
+func WrapKey(keyToWrap []byte, wrappingKey []byte, password []byte, nonce *[24]byte) []byte {
 
 	if wrappingKey == nil && password == nil {
 		panic(errors.New("either password or wrapping_key must be passed"))
@@ -82,7 +103,7 @@ func WrapKey(keyToWrap []byte, wrappingKey []byte, password []byte, nonce [24]by
 	var hWrappingKey [32]byte
 	copy(hWrappingKey[:], wrappingKey[:])
 
-	secretbox.Seal(wrappedKey, keyToWrap, &nonce, &hWrappingKey)
+	secretbox.Seal(wrappedKey, keyToWrap, nonce, &hWrappingKey)
 
 	return wrappedKey
 }
@@ -92,7 +113,7 @@ Unwraps a key using a provided wrapping key. Alternatively, it can derive
 the wrapping key from a password.
 */
 //TODO can nonce be null? if so, delete that from params
-func UnwrapKey(wrappedKey []byte, wrappingKey []byte, password []byte, nonce [24]byte) []byte {
+func UnwrapKey(wrappedKey []byte, wrappingKey []byte, password []byte, nonce *[24]byte) []byte {
 	if password == nil && wrappingKey == nil {
 		panic(errors.New("either password or wrapping_key must be passed"))
 	}
@@ -108,7 +129,12 @@ func UnwrapKey(wrappedKey []byte, wrappingKey []byte, password []byte, nonce [24
 	var hWrappedKey [32]byte
 	copy(hWrappedKey[:], wrappingKey[:])
 
-	secretbox.Open(unwrappedKey, wrappedKey, &nonce, &hWrappedKey)
+	//TODO if Open doesnt accept nil nonce, generate a random one
+	if nonce != nil {
+		secretbox.Open(unwrappedKey, wrappedKey, nonce, &hWrappedKey)
+	} else {
+		secretbox.Open(unwrappedKey, wrappedKey, nil, &hWrappedKey)
+	}
 
 	return unwrappedKey
 }
@@ -121,7 +147,7 @@ type UmbralPublicKey struct {
 /**
 Loads an Umbral public key from bytes.
 */
-func FromBytes(keyBytes []byte, params *UmbralParameters) UmbralPublicKey {
+func PublicKeyFromBytes(keyBytes []byte, params *UmbralParameters) UmbralPublicKey {
 	if params == nil {
 		params = DefaultParams()
 	}
@@ -177,7 +203,7 @@ func FromHex(hexUmbralPubKey string, isCompressed bool) UmbralPublicKey {
 		panic(err)
 	}
 
-	return FromBytes(decodedUmbralPubKey, nil)
+	return PublicKeyFromBytes(decodedUmbralPubKey, nil)
 }
 
 func (uPubKey UmbralPublicKey) ByteString() ([]byte, error) {
@@ -256,7 +282,7 @@ func PrivateKeyFromBytes(keyBytes []byte, wrappingKey *[]byte, password *[]byte,
 	}
 
 	if wrappingKey != nil && password != nil {
-		keyBytes = UnwrapKey(keyBytes, *wrappingKey, *password, *nonce)
+		keyBytes = UnwrapKey(keyBytes, *wrappingKey, *password, nonce)
 	}
 
 	bnKey, err := BytesToModBN(keyBytes, params.Curve)
@@ -279,5 +305,112 @@ func PrivateKeyFromBytes(keyBytes []byte, wrappingKey *[]byte, password *[]byte,
 		BnKey:     *bnKey,
 		PublicKey: pubKey,
 	}
+}
 
+/*
+Returns an UmbralPrivateKey as bytes with optional symmetric
+encryption via nacl's Salsa20-Poly1305.
+If a password is provided instead of a wrapping key, it will use
+Scrypt for key derivation.
+*/
+func (uPrivKey UmbralPrivateKey) ToBytes(wrappingKey *[]byte, password *[]byte, nonce *[24]byte) []byte {
+
+	keyBytes, err := uPrivKey.BnKey.Bytes()
+	if err != nil {
+		panic(err)
+	}
+
+	//TODO do this better
+	if wrappingKey != nil || password != nil {
+		if wrappingKey != nil {
+			keyBytes = WrapKey(keyBytes, *wrappingKey, nil, nonce)
+		} else {
+			keyBytes = WrapKey(keyBytes, nil, *password, nonce)
+		}
+	}
+
+	return keyBytes
+}
+
+func (uPrivKey UmbralPrivateKey) GetPublicKey() UmbralPublicKey {
+	return uPrivKey.PublicKey
+}
+
+func (uPrivKey UmbralPrivateKey) ToCryptographyPrivKey() ecdsa.PrivateKey {
+	//TODO dunno how to implement this, see PyUmbral and let your brain explode
+}
+
+/**
+This type with its methods handles keying material for Umbral,
+by allowing deterministic derivation of UmbralPrivateKeys based on labels.
+Don't use this key material directly as a key.
+*/
+type UmbralKeyingMaterial struct {
+	KeyingMaterial []byte
+}
+
+func NewUmbralKeyingMaterial(keyingMaterial *[]byte) UmbralKeyingMaterial {
+	if keyingMaterial != nil {
+		if len(*keyingMaterial) < 32 {
+			panic(errors.New("UmbralKeyingMaterial must have size at least 32 bytes"))
+		}
+		return UmbralKeyingMaterial{
+			KeyingMaterial: *keyingMaterial,
+		}
+	} else {
+		casual := make([]byte, 64)
+		_, err := rand.Read(casual)
+		if err != nil {
+			panic(err)
+		}
+		return UmbralKeyingMaterial{
+			KeyingMaterial: casual,
+		}
+	}
+}
+
+func newHash() hash.Hash {
+	hashh, err := blake2b.New(blake2b.Size, nil)
+	if err != nil {
+		panic(err)
+	}
+	return hashh
+}
+
+func (uKeyMat UmbralKeyingMaterial) DerivePrivKeyByLabel(label []byte, salt []byte, params *UmbralParameters) UmbralPrivateKey {
+
+	if params == nil {
+		params = DefaultParams()
+	}
+
+	//TODO dunno what secret is, if it is a random byte array to bring more security or something more specifical
+	keyMaterial, err := hkdf.New(newHash, nil, salt, label).Read(uKeyMat.KeyingMaterial)
+	if err != nil {
+		panic(err)
+	}
+
+	bnKey, err := HashToModBN(keyMaterial, params)
+	if err != nil {
+		panic(err)
+	}
+
+	return UmbralPrivateKey{}
+}
+
+func KeyingMaterialFromBytes(keyBytes []byte, wrappingKey []byte, password []byte, nonce *[24]byte) UmbralKeyingMaterial {
+	if wrappingKey != nil && password != nil {
+		keyBytes = UnwrapKey(keyBytes, wrappingKey, password, nonce)
+	}
+	return UmbralKeyingMaterial{KeyingMaterial: keyBytes}
+}
+
+func (uKeyMat UmbralKeyingMaterial) ToBytes(wrappingKey []byte, password []byte, nonce *[24]byte) []byte {
+
+	keyBytes := uKeyMat.KeyingMaterial
+
+	if wrappingKey != nil && password != nil {
+		keyBytes = WrapKey(keyBytes, wrappingKey, password, nonce)
+	}
+
+	return keyBytes
 }
